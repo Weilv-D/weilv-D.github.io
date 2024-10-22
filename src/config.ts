39b244e821ff1c29c6963f1a7e661efd25345936
weilv <weilv@mail.com>
2024-10-22 19:04:59 +0800

    feat: 更新Giscus配置并优化首页文本

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,21 @@ export const Socials: Record<string, Record<string, string>> = {
 
 // doc: https://giscus.app
 // data-theme is auto changed between noborder_light / noborder_gray
-export const GiscusConfig: Record<string, string> = {};
+export const GiscusConfig: Record<string, string> = {
+	'data-repo': 'ladit/astro-blog-zozo',
+	'data-repo-id': 'R_kgDOLgobXQ',
+	'data-category': 'Announcements',
+	'data-category-id': 'DIC_kwDOLgobXc4Cd_N6',
+	'data-mapping': 'pathname',
+	'data-strict': '0',
+	'data-reactions-enabled': '1',
+	'data-emit-metadata': '0',
+	'data-input-position': 'top',
+	'data-lang': 'zh-CN',
+	'data-loading': 'lazy',
+	crossorigin: 'anonymous',
+	async: '',
+};
 
 export type HideElements =
 	| PostHideElements
